2743b7f34b1e85bc240df8fabd6d895c3646a975
Zhanbol <bollzhan4@gmail.com>
2025-02-03 18:49:51 +0100

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,37 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import { Manrope, DM_Sans } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
+import './globals.css'
+import { ThemeProvider } from '@/components/theme'
+import { Toaster } from 'sonner'
+
+const manrope = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Opal",
-  description: "",
-};
+  title: 'Opal',
+  description: 'Share AI powered videos with your friends.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${manrope.className} bg-[#171717]`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+                {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
