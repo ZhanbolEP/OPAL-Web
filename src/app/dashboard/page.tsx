5a976c1e28012e77c54b5852a36dad7823eb79e0
Zhanbol <bollzhan4@gmail.com>
2025-02-05 11:28:39 +0100

    further development

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardPage = (props: Props) => {
+    //Auth
+
+    //If accoutn   does not exists
+    return <div>DashboardPage</div>
+} 
+
+
+export default DashboardPage
